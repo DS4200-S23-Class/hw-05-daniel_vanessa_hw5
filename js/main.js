039164319b571b4097c399cfbfbf8cd1b60c9348
liangv21 <liang.va@northeastern.edu>
2023-02-21 23:13:53 -0500

    border woo

--- js/main.js
+++ js/main.js
@@ -31,23 +31,22 @@ d3.csv("data/scatter-data.csv").then((data) => {
             .attr("cy", (d) => { return ((Y_FIX - parseInt(d.y)) * SCATTER_FACTOR) + MARGINS.top; })
             .attr("r", 15);
       
-    addEventHandler()    
+    addEventHandler();
 
 });
 
 // create button function to add a new point
 function newPoint() {
-  let x_coordinate = document.getElementById("x-coordinate");
-  let y_coordinate = document.getElementById("y-coordinate");
+  let x_coordinate = document.getElementById("x-coordinate").value;
+  let y_coordinate = document.getElementById("y-coordinate").value;
 
   FRAME1.append("circle")
           .attr("class", "point")
-          .attr("cx", (x_coordinate.value * SCATTER_FACTOR) + MARGINS.left)
-          .attr("cy", ((Y_FIX - y_coordinate.value) * SCATTER_FACTOR) + MARGINS.top)
+          .attr("cx", (x_coordinate * SCATTER_FACTOR) + MARGINS.left)
+          .attr("cy", ((Y_FIX - y_coordinate) * SCATTER_FACTOR) + MARGINS.top)
           .attr("r", 15);
-  console.log(x_coordinate.value, y_coordinate.value)
 
-  addEventHandler()
+  addEventHandler();
 };
 
 // mouseover function for adding highlight
@@ -63,7 +62,21 @@ function pointMouseout(event, d) {
 // click function (does not work properly yet!!)
 function pointClick(event, d) {
   const clicked = d3.select(this);
-  if (clicked.classed("selected")) {
+  const clicked_x = (clicked.attr("cx") - MARGINS.left) / SCATTER_FACTOR;
+  const clicked_y = Y_FIX - ((clicked.attr("cy") - MARGINS.top) / SCATTER_FACTOR);
+
+  // update last clicked point
+  d3.select("#coordinates").text("Last point clicked: (" + clicked_x + "," + clicked_y + ")")
+
+  // add/remove border
+  if (clicked.style("stroke") == "black") {
+    clicked.style("stroke", "none");
+  } else {
+    clicked.style("stroke-width", 3);
+    clicked.style("stroke", "black");
+  };
+  
+  /* if (clicked.classed("selected")) {
       clicked.classed("selected", false);
       d3.select("#coordinates").text("Last point clicked:");
       clicked.attr("stroke", null);
@@ -71,7 +84,7 @@ function pointClick(event, d) {
       clicked.classed("selected", true);
       d3.select("#coordinates").text("Last point clicked: " + `(${d.x}, ${d.y})`);
       clicked.attr("stroke", "black").attr("stroke-width", 2);
-    }
+    } */
 };
 
 // function to add event handlers to points
